e80082c5dec4ff8a9773a7f0a5ab80173ff95003
Paul Naranja <Paul@pauls-mbp.attlocal.net>
2015-07-02 23:51:13 -0700

    Finished another Bonfire

---
+++ Bonfires/SeekDestroy.js
@@ -0,0 +1,13 @@
+function destroyer(arr){ 
+    var new_arr = arr;
+    var args = arguments;
+    
+    for (var i=1; i<=arguments.length-1; i++)
+        new_arr = new_arr.filter(function(elem){
+            return elem != args[i];
+        });
+    
+    return new_arr;
+     }
+
+destroyer([1,2,3],2);
