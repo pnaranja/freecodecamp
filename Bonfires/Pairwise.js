66da618c09ff36856ab851cdb8fccdb244b13c39
pnaranja <paul.naranja@gmail.com>
2015-07-06 00:28:09 -0700

    Finished Pairwise and working on Function Programming exercises

--- Bonfires/Pairwise.js
+++ Bonfires/Pairwise.js
@@ -1,19 +1,22 @@
 function pairwise(arr, arg) {
-  var sum_of_indices;
+  var indices_obj = {}; //{ [array of found values] : sum }
+  var sum = 0;
   var indices_taken =  [];
   
-  var already_taken = function(index){
+  var already_taken = function(idx){
     return indices_taken.some(function(element,index,array)
-                              {return element === index;});
+                              {return element === idx;});
   };
-  
-  var find_pair = function(a,b){
-    if(a+b===arg){
-      
+
+  for (var i=0;i<arr.length-2;i++)
+    for(var j=1;j<arr.length-1;j++){
+      if( (!already_taken(i) && !already_taken(j)) && (i!=j) && (arr[i]+arr[j]===arg) ){
+        indices_taken.push(i);
+        indices_taken.push(j);
+        sum += i + j;
+      }
     }
-  };
-  
-  return arg;
+  return sum;
 }
 
 pairwise([1,4,2,3,0,5], 7);
