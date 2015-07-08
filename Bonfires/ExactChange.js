8513793323a3c89f9cbac3b5fd9c5ec0259b2e5e
pnaranja <paul.naranja@gmail.com>
2015-07-07 17:54:25 -0700

    Start Exact Change bonfire

---
+++ Bonfires/ExactChange.js
@@ -0,0 +1,22 @@
+function drawer(price, cash, cid) {
+  var total_cid = function(){
+    var reduced_arr =  cid.reduce(function(prev,curr){
+      return new Array('',prev[1]+curr[1]);
+    });
+    return reduced_arr[1];
+  };
+  
+}
+
+// Example cash-in-drawer array:
+// [['PENNY', 1.01],
+// ['NICKEL', 2.05],
+// ['DIME', 3.10],
+// ['QUARTER', 4.25],
+// ['ONE', 90.00],
+// ['FIVE', 55.00],
+// ['TEN', 20.00],
+// ['TWENTY', 60.00],
+// ['ONE HUNDRED', 100.00]]
+
+//drawer(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]);
