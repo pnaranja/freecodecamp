9d19698983b3fba8f86f2b88b32b47211f14a05c
pnaranja <paul.naranja@gmail.com>
2015-07-10 12:10:47 -0700

    Finished Bonfire

--- Bonfires/ExactChange.js
+++ Bonfires/ExactChange.js
@@ -22,13 +22,13 @@ function drawer(price, cash, cid) {
   if (change === total_cid())
     return 'Closed';
 
-  var denom_value = {'PENNY':.01, 'NICKEL':.05, 'DIME':.10, 'QUARTER':.25, 'ONE':1, 'FIVE':5, 'TEN':10, 'TWENTY':20, 'ONE HUNDRED':100};
+  var denom_value = {'PENNY':0.01, 'NICKEL':0.05, 'DIME':0.10, 'QUARTER':0.25, 'ONE':1, 'FIVE':5, 'TEN':10, 'TWENTY':20, 'ONE HUNDRED':100};
 
   //Find out largest denomination for change
   var greatest_denom = function(){
-    if (change<.05) return 'PENNY';
-    if (change<.10) return 'NICKEL';
-    if (change<.25) return 'DIME';
+    if (change<0.05) return 'PENNY';
+    if (change<0.10) return 'NICKEL';
+    if (change<0.25) return 'DIME';
     if (change<1) return 'QUARTER';
     if (change<5) return 'ONE';
     if (change<10) return 'FIVE';
@@ -50,29 +50,21 @@ function drawer(price, cash, cid) {
   // - Subtract denomination from change
   // - Subtract denomination from amt of denomination
   // - Create array of ['denom', amt subtracted]
-  // **Using trampoline method so stack does not get overloaded**
-
-  var trampoline = function(fn){
-    while (fn instanceof Function){
-      fn = fn();
-    }
-    return fn;
-  };
 
   var denom_change = function(denom_arr){
         var denom = denom_arr[0];
 
         var acc = function(denom_arr, ret_arr){
-            if (change === 0 || denom_arr[1] === 0){ 
-                console.log(ret_arr);
+            if (change <= 0 || denom_arr[1] === 0 || change < denom_value[denom]){ 
                 return ret_arr;
                 
             }
-            change = change - denom_value[denom];
+            console.log(change, denom_value[denom]);
+            change = Math.round((change - denom_value[denom])*100)/100;
             return acc([denom, denom_arr[1]-denom_value[denom]],
                     [ret_arr[0], ret_arr[1]+denom_value[denom]]);
         };
-        return trampoline(acc(denom_arr, [denom,0]));
+        return acc(denom_arr, [denom,0]);
   };
 
 
@@ -87,10 +79,10 @@ function drawer(price, cash, cid) {
       total_change.push(denom_change(current_denom[i]));
   }
   
-  return total_change;
-  //return denom_change(['QUARTER', 4.25]);
+  return total_change.filter(function(arr){ return arr[1] !== 0});
 
 }
 
 
-drawer(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]);
+drawer(3.26, 100.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.01]]);
+
