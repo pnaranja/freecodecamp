76e3189a124901d5c3d78a08de3288bdbf8fab34
pnaranja <paul.naranja@gmail.com>
2015-12-03 22:11:10 -0800

    Created objects

--- Bonfires/RomanNumeralConverter.js
+++ Bonfires/RomanNumeralConverter.js
@@ -17,5 +17,16 @@
  * 
  */
 function convert(num){
+    var Characters = {
+        {1SDP : {{Small:"I"}, {Mid:"V"}, {Large:"X"}}
+        {100SDP : {{Small:"X"}, {Mid:"L"}, {Large:"C"}}
+        {100SDP : {{Small:"C"}, {Mid:"D"}, {Large:"M"}}
+        {1000SDP : {{Small:"M"}, {Mid:"M"}, {Large:"M"}}};
+
+    var Ranges = {
+        {1:[Small]}, {2:[Small,Small]}, {3:[Small,Small,Small]}
+        {4:[Small,Mid]}, {5:[Mid]}, {6:[Mid,Small]}, {7:[Mid,Small,Small]}, {8:[Mid,Small,Small,Small]}
+        {9:[Mid,Large]}};
+
     return num;
 }
